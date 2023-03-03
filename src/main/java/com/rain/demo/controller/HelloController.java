da1659bfd4de57df54672bda7efd590b680c6a4c
rainxxx <puhan@guoshikeji.onaliyun.com>
2023-03-03 10:02:25 +0800

    测试三

--- src/main/java/com/rain/demo/controller/HelloController.java
+++ src/main/java/com/rain/demo/controller/HelloController.java
@@ -1,7 +1,15 @@
 package com.rain.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 public class HelloController {
+  
+    @RequestMapping
+    public String hello(){
+        return "hello";
+    }
+  //HELLO
 }
